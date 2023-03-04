6eab5d38866a930f20c85df8d54d919127553765
superbot <test@localhost>
2023-03-04 22:40:07 +0300

    fix server redirect

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const HOSTNAME = process.env.HOSTNAME || 'localhost';
 
 app.use(express.static(`${__dirname}/dist/`));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Start web server on http://${HOSTNAME}:${PORT}`);
