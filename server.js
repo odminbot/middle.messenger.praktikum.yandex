79db982a125b1d17823b60b325c8440c9e28d4a4
odminbot <odminbot@gmail.com>
2023-03-23 20:24:38 +0300

    fix stylelint error

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const HOSTNAME = process.env.HOSTNAME || 'localhost';
 app.use(express.static(`${__dirname}/dist/`));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 app.listen(PORT, () => {
